b97de28fc4bfe1b53189d24e5a0bb3cf1515daad
Rafael Martínez Montiel <rafael@payclip.com>
2015-06-30 17:56:38 -0500

    Remove unnecesary for

--- tumblr/tumblrimgs.sh
+++ tumblr/tumblrimgs.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 FILES=` curl http://api.tumblr.com/v2/blog/$1/posts/photo?notes_info=true | jq '.response.posts[].photos[].original_size.url ' | sed  -e 's/"//g'`
-for FILE in $FILES
- do echo $FILE 
- wget  --timestamping --directory-prefix=$2 $FILE 
-done
+wget  --timestamping --directory-prefix=$2 $FILES
