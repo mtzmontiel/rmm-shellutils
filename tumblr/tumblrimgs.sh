80da08c4ddc993858d2d868c180456fdf7e02f7b
Rafael Martínez Montiel <rafael@payclip.com>
2015-07-07 10:43:26 -0500

    Offset para recuperar blogs pasados

--- tumblr/tumblrimgs.sh
+++ tumblr/tumblrimgs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-FILES=` curl http://api.tumblr.com/v2/blog/$1/posts/photo?notes_info=true | jq '.response.posts[].photos[].original_size.url ' | sed  -e 's/"//g'`
+FILES=` curl http://api.tumblr.com/v2/blog/$1/posts/photo?notes_info=true\&offset=$3 | jq '.response.posts[].photos[].original_size.url ' | sed  -e 's/"//g'`
 wget  --timestamping --directory-prefix=$2 $FILES
